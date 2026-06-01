cd84f930a42efa1d1dedc37d0eec94b1ee099b4d
Quentin pc fix <quentinmontartgis6@gmail.com>
2026-06-01 10:11:16 +0200

    sa me pete les couille

---
+++ WpfApp3/ChoixAdversaire.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    public partial class ChoixAdversaireWindow : Window
+    {
+        public ChoixAdversaireWindow()
+        {
+            InitializeComponent();
+
+            // MAGIE : La fenêtre lit la police du cerveau. 
+            // Tous les textes à l'intérieur vont s'adapter d'un coup !
+            this.FontSize = ConfigurationJeu.TaillePolice;
+        }
+
+        private void BtnLancer_Click(object sender, RoutedEventArgs e)
+        {
+            // On sauvegarde le choix de l'adversaire
+            if (RadRobot.IsChecked == true)
+            {
+                ConfigurationJeu.TypeAdversaire = "Robot";
+            }
+            else
+            {
+                ConfigurationJeu.TypeAdversaire = "Humain";
+            }
+
+            MessageBox.Show($"Partie lancée contre : {ConfigurationJeu.TypeAdversaire} !\n" +
+                            $"Couleur de vos pions : {ConfigurationJeu.CouleurPieces}");
+
+            // Ici, tu ouvriras ta future fenêtre de jeu "GrillePuissance4Window"
+            this.Close();
+        }
+    }
+}
---
+++ WpfApp3/ConfigurationJeu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp3
+{
+    public static class ConfigurationJeu
+    {
+        // Ce fichier va stocker tes choix en mémoire d'une page à l'autre
+        public static double TaillePolice { get; set; } = 14;
+        public static string CouleurPieces { get; set; } = "Rouge";
+        public static string FormePieces { get; set; } = "Rond";
+        public static string CouleurGrille { get; set; } = "Bleu";
+        public static string TypeAdversaire { get; set; } = "Humain";
+    }
+}
